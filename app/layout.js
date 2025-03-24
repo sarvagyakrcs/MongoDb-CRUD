5f559acc4361f0ea63a413a6aa52fd31e1bbe538
Sarvagya Kumar <sarvagyakrcs@gmail.com>
2025-03-24 20:50:22 +0530

    fix(layout): correct font weight format in layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,9 @@
-import { Geist } from "next/font/google";
+import { Geist } from 'next/font/google';
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import QueryClientProvider from "@/lib/query-client-provider";
 
-const font = Geist({ weight: 400, subsets: ["latin"] });
+const font = Geist({ weight: "400", subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
